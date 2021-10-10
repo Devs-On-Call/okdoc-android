24dba6b9f61ce22bbb86376da3032830e11e4f9f
Jim Skretas <jimskretas@gmail.com>
2021-10-10 20:22:35 +0300

    Refactor bottom nav to use navController & navGraph (#5)
    - Refactor bottom navigation to be the same with the template one
        - Add nav graph
        - Use nav controller to create the bottom navigation instead of manually changing fragments
    - Add functionality to the Prescriptions button in Home Fragment (temp)
    - Add functionality to the Back button in Prescription Fragment (temp)

--- app/src/main/java/com/devsoncall/okdoc/activities/MainMenuActivity.kt
+++ app/src/main/java/com/devsoncall/okdoc/activities/MainMenuActivity.kt
@@ -1,63 +1,24 @@
 package com.devsoncall.okdoc.activities
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.devsoncall.okdoc.R
-import com.devsoncall.okdoc.fragments.HomeFragment
-import com.devsoncall.okdoc.fragments.PrescriptionFragment
-import com.devsoncall.okdoc.fragments.ProfileFragment
-import com.devsoncall.okdoc.fragments.SettingsFragment
-import kotlinx.android.synthetic.main.activity_main_menu.*
-import kotlinx.android.synthetic.main.home_fragment.*
-import kotlinx.android.synthetic.main.prescription_fragment.*
+
 
 class MainMenuActivity : AppCompatActivity() {
-
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
-        val homeFragment = HomeFragment()
-        val profileFragment = ProfileFragment()
-        val settingsFragment = SettingsFragment()
-        val prescriptionFragment = PrescriptionFragment()
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
 
-
-        // default fragment on activity creation
-        setCurrentFragment(homeFragment)
-
-        bottomNavigationView.setOnNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.home -> setCurrentFragment(homeFragment)
-                R.id.profile -> setCurrentFragment(profileFragment)
-                R.id.settings -> setCurrentFragment(settingsFragment)
-            }
-            true
-        }
-
-//        homeFragment.buttonPrescriptions.setOnClickListener {
-////            setCurrentFragment(prescriptionFragment)
-//        }
     }
-
-    fun test() {
-        Toast.makeText(this, "yay", Toast.LENGTH_LONG).show()
-    }
-
-    private fun setCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.frameLayoutFragment, fragment)
-            addToBackStack(null)
-            commit()
-        }
 
 }
 
