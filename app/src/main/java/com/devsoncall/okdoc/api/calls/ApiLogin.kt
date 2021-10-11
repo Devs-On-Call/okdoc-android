22e7e5c97c683ae0718d1b4441642ef59bd636f7
jimskretas <jimskretas@gmail.com>
2021-10-11 16:49:14 +0300

    Extract retrofit calls into separate classes

---
+++ app/src/main/java/com/devsoncall/okdoc/api/calls/ApiLogin.kt
@@ -0,0 +1,37 @@
+package com.devsoncall.okdoc.api.calls
+
+import com.devsoncall.okdoc.api.RetrofitClient
+import com.devsoncall.okdoc.models.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ApiLogin {
+
+    private var mListener: DataInterface? = null
+
+    fun login(amka: String) {
+        val call: Call<BasicResponse>? =
+            RetrofitClient().getInstance()?.getApi()?.createToken(amka)
+
+        call!!.enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(
+                call: Call<BasicResponse>,
+                response: Response<BasicResponse>
+            ) {
+                mListener?.responseData(response)
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {}
+        })
+    }
+
+    fun setOnDataListener(listener: DataInterface) {
+        mListener = listener
+    }
+
+    interface DataInterface {
+        fun responseData(loginResponse: Response<BasicResponse>)
+    }
+
+}
