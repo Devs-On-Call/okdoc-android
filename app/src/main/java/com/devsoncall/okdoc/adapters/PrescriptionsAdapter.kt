34ef796117212b039257783eed6bba059f6a5d87
marinakip <marinakipourou@hotmail.com>
2021-10-12 19:20:51 +0300

    add OnClickListener in the prescriptionsViewHolder inside the PrescriptionsAdapter add onItemClick function in the PrescriptionListFragment

--- app/src/main/java/com/devsoncall/okdoc/adapters/PrescriptionsAdapter.kt
+++ app/src/main/java/com/devsoncall/okdoc/adapters/PrescriptionsAdapter.kt
@@ -1,5 +1,6 @@
 package com.devsoncall.okdoc.adapters
 
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,13 +9,13 @@ import com.devsoncall.okdoc.R
 import kotlinx.android.synthetic.main.item_prescription.view.*
 
 class PrescriptionsAdapter(
-    private val prescriptions: List<PrescriptionsElements>
+    private val prescriptions: List<PrescriptionsElements>,
+    private val listener: OnItemClickListener
 ) : RecyclerView.Adapter<PrescriptionsAdapter.PrescriptionsViewHolder>() {
-
-    inner class PrescriptionsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PrescriptionsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_prescription, parent, false)
+
         return PrescriptionsViewHolder(view)
     }
 
@@ -24,11 +25,31 @@ class PrescriptionsAdapter(
             tvDoctorDetails.text = prescriptions[position].doctor
         }
 
+
     }
 
     override fun getItemCount(): Int {
         return prescriptions.size
     }
 
+
+    inner class PrescriptionsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = bindingAdapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                if (v != null) {
+                    listener.onItemClick(position, v)
+                }
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int, view: View)
+    }
 
 }
--- app/src/main/java/com/devsoncall/okdoc/fragments/PrescriptionListFragment.kt
+++ app/src/main/java/com/devsoncall/okdoc/fragments/PrescriptionListFragment.kt
@@ -5,16 +5,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.devsoncall.okdoc.R
 import com.devsoncall.okdoc.adapters.PrescriptionsAdapter
 import com.devsoncall.okdoc.adapters.PrescriptionsElements
-import kotlinx.android.synthetic.main.item_prescription.*
-import kotlinx.android.synthetic.main.prescriptions_list_fragment.*
 
-class PrescriptionListFragment : Fragment(R.layout.prescriptions_list_fragment) {
+class PrescriptionListFragment : Fragment(R.layout.prescriptions_list_fragment), PrescriptionsAdapter.OnItemClickListener {
 
         override fun onCreateView(
             inflater: LayoutInflater,
@@ -38,19 +40,33 @@ class PrescriptionListFragment : Fragment(R.layout.prescriptions_list_fragment) 
                 PrescriptionsElements("17/09/2019", "Dr. Stella Sotiriou - Pathologist")
             )
 
-            val adapter = PrescriptionsAdapter(prescriptionsList)
+            val adapter = PrescriptionsAdapter(prescriptionsList, this)
             val rvPrescriptions = view.findViewById<RecyclerView>(R.id.rvPrescriptions)
             rvPrescriptions.adapter = adapter
-            rvPrescriptions.layoutManager = LinearLayoutManager(context)
+            rvPrescriptions.layoutManager = LinearLayoutManager(this.context)
 
-//            item_layout.setOnClickListener{
-//                // when clicked it should lead to prescription_fragment
-//                adapter.notifyDataSetChanged()
+
+            val buttonBack = view.findViewById<Button>(R.id.btBack)
+
+            buttonBack.setOnClickListener { view ->
+                view.findNavController().navigate(R.id.navigation_home)
+            }
+
+//            val itemLayout = view.findViewById<LinearLayout>(R.id.item_layout)
 //
+//            itemLayout.setOnClickListener{
+//                //Toast.makeText(this.context, "It clicked!! ", Toast.LENGTH_LONG).show()
+//                view -> view.findNavController().navigate(R.id.navigation_prescription)
 //            }
 
-
-
         }
+
+        override fun onItemClick(position: Int, view: View) {
+            view.findNavController().navigate(R.id.navigation_prescription)
+           // Toast.makeText(this.context, "Item $position clicked", Toast.LENGTH_SHORT).show()
+
+//
+        }
+
 
 }
