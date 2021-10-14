a28175f93f223998ace8c91b98384900fd65aa36
marinakip <marinakipourou@hotmail.com>
2021-10-14 21:17:46 +0300

    add bind diagnoses list with the Api

--- app/src/main/java/com/devsoncall/okdoc/adapters/DiagnosesAdapter.kt
+++ app/src/main/java/com/devsoncall/okdoc/adapters/DiagnosesAdapter.kt
@@ -1,14 +1,17 @@
 package com.devsoncall.okdoc.adapters
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.devsoncall.okdoc.R
+import com.devsoncall.okdoc.models.Diagnosis
 import kotlinx.android.synthetic.main.item_diagnosis.view.*
 
 class DiagnosesAdapter(
-    private val diagnoses: List<DiagnosesElements>,
+    private val diagnoses: List<Diagnosis>,
     private val listener: OnItemClickListener
 ) : RecyclerView.Adapter<DiagnosesAdapter.DiagnosesViewHolder>() {
 
@@ -18,8 +21,11 @@ class DiagnosesAdapter(
         return DiagnosesViewHolder(view)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: DiagnosesViewHolder, position: Int) {
         holder.itemView.apply {
+            // we need to parse this date and get only YY-MM-DD
+            // val date: Date = Date.from(Instant.parse(diagnoses[position].date))
             tvDate.text = diagnoses[position].date
             tvDiagnosis.text = diagnoses[position].diagnosis
         }
