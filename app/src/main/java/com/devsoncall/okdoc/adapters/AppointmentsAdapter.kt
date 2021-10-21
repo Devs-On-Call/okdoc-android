da4825acfb60238252310009883dcd5756002fe8
ch-ant <ant.chr@outlook.com>
2021-10-22 00:07:29 +0300

    improve view appointments
    - add more information about past appointments
    - format date
    - improve text format overall

--- app/src/main/java/com/devsoncall/okdoc/adapters/AppointmentsAdapter.kt
+++ app/src/main/java/com/devsoncall/okdoc/adapters/AppointmentsAdapter.kt
@@ -9,6 +9,8 @@ import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.devsoncall.okdoc.R
 import com.devsoncall.okdoc.models.PatientAppointment
+import com.devsoncall.okdoc.utils.formatDateString
+import com.devsoncall.okdoc.utils.getDayOfWeek
 import kotlinx.android.synthetic.main.item_appointment.view.*
 import kotlinx.android.synthetic.main.item_prescription.view.tvDate
 import kotlinx.android.synthetic.main.item_prescription.view.tvDoctorDetails
@@ -28,14 +30,20 @@ class AppointmentsAdapter(
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: AppointmentsViewHolder, position: Int) {
         holder.itemView.apply {
-//            we need to parse this date and get only YY-MM-DD
-//            val date: Date = Date.from(Instant.parse(prescriptions[position].date))
-            tvHospital.text = appointments[position].hospital.name
-            tvDate.text = appointments[position].date
 
             val doctorName = appointments[position].doctor.name
             val doctorLastName = appointments[position].doctor.lastName
-            tvDoctorDetails.text = "Dr. $doctorName $doctorLastName"
+            val doctorSpecialty = appointments[position].doctor.profession.name
+            val hospitalName = appointments[position].hospital.name
+            val hospitalLocation = appointments[position].hospital.address
+            val date = appointments[position].date.take(10)
+            val dayOfWeek = getDayOfWeek(date).toString()
+
+            textViewDoctorName.text = "Dr. $doctorName $doctorLastName"
+            textViewDoctorSpecialty.text = doctorSpecialty
+            textViewHospitalName.text = "@ $hospitalName"
+            textViewHospitalLocation.text = hospitalLocation
+            textViewDate.text = formatDateString(date, dayOfWeek)
         }
     }
 
